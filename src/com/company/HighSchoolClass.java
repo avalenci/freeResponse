a3a609f14dcd154aebabec257d27d9ac5224d372
avalenci <ansmeew@gmail.com>
2016-11-10 23:48:07 -0500

    Second commit.

--- src/com/company/HighSchoolClass.java
+++ src/com/company/HighSchoolClass.java
@@ -1,5 +1,36 @@
 package com.company;
 
 public class HighSchoolClass {
-    private Student [] students;
+    private void students(double[] x) {
+        gpa = x;
+    }
+    public double getValedictorian() {
+        int count = 0;
+        double a;
+        double b = -1;
+        while (count < gpa.length) {
+            a = gpa[count];
+            if (a > b) {
+                b = a;
+            }
+            count++;
+        }
+        return b;
+    }
+    public double getHonorsPercentage() {
+        double count = 0;
+        while (count < gpa.length) {
+        }
+        return 0;
+    }
+    public double getGPA(double z) {
+        return z;
+    }
+    public boolean isHonors(double y) {
+        if (y >= 3)
+            return true;
+        else
+            return false;
+    }
+    public double[] gpa;
 }
